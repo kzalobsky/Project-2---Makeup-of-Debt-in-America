84f3ec9cf5050a981d207bb6d7c424bba3c6d8e7
Rilee Peebles <rlynnpeebles@gmail.com>
2022-12-08 17:36:37 -0500

    Combining code

--- Analysis/Makeup_Of_Debt_in_America.sql
+++ Analysis/Makeup_Of_Debt_in_America.sql
@@ -1,37 +1,34 @@
+------- Rilee's Data
 
+DROP TABLE IF EXISTS renamed_state_debt;
 
+CREATE TABLE renamed_state_debt (
+	state TEXT,
+	mortgage INT,
+	home_equity INT,
+	auto_loan INT, 
+	credit_card INT,
+	student_loan INT,
+	other INT
+);
 
+SELECT * FROM renamed_state_debt;
 
+DROP TABLE IF EXISTS renamed_debt_age;
 
+CREATE TABLE renamed_debt_age (
+	loan_type TEXT,
+	"18-29" REAL,
+	"30-39" REAL,
+	"40-49" REAL,
+	"50-59" REAL,
+	"60-69" REAL,
+	"70+" REAL
+);
 
+SELECT * FROM renamed_debt_age;
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+------- Kirsten's Data
 
 DROP TABLE us_gdp_df;
 DROP TABLE debt_balance_and_comp_df;
